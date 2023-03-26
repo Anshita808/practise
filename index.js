fd6ad9eeecb1e3ab3b4f48484797200aa9d08dd6
Anshita <anshitagupta783@gmail.com>
2023-03-27 01:19:36 +0530

    done

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const {connection}=require("./db");
+const {userRouter}=require("./route/user.route");
+const {noteRoute}=require("./route/note.route");
+const { auth } = require("./middleware/note.middleware");
+const cors = require("cors");
+
+
+
+const app = express();
+
+require("dotenv").config();
+
+
+app.use(cors());
+app.use(express.json());
+app.use("/user",userRouter);
+app.use(auth);
+app.use("/note",noteRoute);
+
+app.listen(process.env.PORT,async()=>{
+    try {
+        await connection
+        console.log("Connected to db");
+    } catch (error) {
+        console.log(error);
+    }
+    console.log("server is running");
+})
